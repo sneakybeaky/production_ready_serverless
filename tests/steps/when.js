73c7e46fc46018a33ddd26d557d7cd5e07bf7bea
Jon Barber <jon.barber@acm.org>
2020-05-13 09:33:38 +0100

    add acceptance tests

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -11,12 +11,64 @@ const viaHandler = async (event, functionName) => {
 
     const context = {}
     const response = await handler(event, context)
-    const contentType = _.get(response, 'headers.Content-Type', 'application/json');
+    const contentType = _.get(response, 'headers.content-type', 'application/json');
     if (response.body && contentType === 'application/json') {
         response.body = JSON.parse(response.body);
     }
     return response
 }
+
+const respondFrom = async (httpRes) => ({
+    statusCode: httpRes.status,
+    body: httpRes.data,
+    headers: httpRes.headers
+})
+
+const signHttpRequest = (url) => {
+    const urlData = URL.parse(url)
+    const opts = {
+        host: urlData.hostname,
+        path: urlData.pathname
+    }
+
+    aws4.sign(opts)
+    return opts.headers
+}
+
+const viaHttp = async (relPath, method, opts) => {
+    const url = `${process.env.rootUrl}/${relPath}`
+    console.info(`invoking via HTTP ${method} ${url}`)
+
+    try {
+        const data = _.get(opts, "body")
+        let headers = {}
+        if (_.get(opts, "iam_auth", false) === true) {
+            headers = signHttpRequest(url)
+        }
+
+        const authHeader = _.get(opts, "auth")
+        if (authHeader) {
+            headers.Authorization = authHeader
+        }
+
+        const httpReq = http.request({
+            method, url, headers, data
+        })
+
+        const res = await httpReq
+        return respondFrom(res)
+    } catch (err) {
+        if (err.status) {
+            return {
+                statusCode: err.status,
+                headers: err.response.headers
+            }
+        } else {
+            throw err
+        }
+    }
+}
+
 
 const we_invoke_get_index = async () => {
     switch (mode) {
